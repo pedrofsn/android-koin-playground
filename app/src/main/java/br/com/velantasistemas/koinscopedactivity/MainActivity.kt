6a93509dde4e5b936f8e45b0ce2d204b4d910c72
pedrofsn <pedrokra@gmail.com>
2023-01-09 15:43:41 -0300

    Creating main activity

---
+++ app/src/main/java/br/com/velantasistemas/koinscopedactivity/MainActivity.kt
@@ -0,0 +1,23 @@
+package br.com.velantasistemas.koinscopedactivity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.velantasistemas.koinscopedactivity.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
