781458e347a47178432cf8027da74f4feddbcab5
pedrofsn <pedrokra@gmail.com>
2023-01-09 16:06:51 -0300

    Improving UI

--- app/src/main/java/br/com/velantasistemas/koinscopedactivity/TrickyActivity.kt
+++ app/src/main/java/br/com/velantasistemas/koinscopedactivity/TrickyActivity.kt
@@ -1,7 +1,7 @@
 package br.com.velantasistemas.koinscopedactivity
 
 import android.os.Bundle
-import android.widget.Toast
+import br.com.velantasistemas.koinscopedactivity.databinding.ActivityTrickyBinding
 import java.util.Calendar
 import org.koin.android.ext.android.get
 import org.koin.androidx.scope.ScopeActivity
@@ -9,17 +9,21 @@ import org.koin.core.parameter.parametersOf
 
 class TrickyActivity : ScopeActivity(), MVP.View {
 
+    private lateinit var binding: ActivityTrickyBinding
     private val presenter by lazy { get<MVP.Presenter> { parametersOf(this@TrickyActivity) } }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityTrickyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         presenter.toast()
     }
 
     override fun toast() {
         val id = intent.getIntExtra("id", -1)
-        val text = "TrickyActivity#${id} => ${Calendar.getInstance()}"
-        System.out.println(text)
-        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+        val text = "TrickyActivity#${id}\n${Calendar.getInstance()}"
+        binding.textView.text = text
     }
 }
